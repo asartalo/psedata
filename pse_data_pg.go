fcb3cf77d11023c37e4278dd98bbf49b160a498f
Wayne Duran <asartalo@gmail.com>
2014-07-30 00:28:41 +0800

    Creating the db.

--- pse_data_pg.go
+++ pse_data_pg.go
@@ -1,6 +1,11 @@
 package pse_data_pg
 
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/asartalo/pq"
+	"time"
+)
 
 type DataRow struct {
 	symbol string
@@ -11,3 +16,55 @@ type DataRow struct {
 	close  float64
 	vol    int
 }
+
+type ConnectionInfo struct {
+	dbname   string
+	user     string
+	password string
+	host     string
+	port     int
+}
+
+func (info ConnectionInfo) ConnectStringTemplateDb() string {
+	return info.connectStringGeneral("template1")
+}
+
+func (info ConnectionInfo) ConnectString() string {
+	return info.connectStringGeneral(info.dbname)
+}
+
+func (info ConnectionInfo) connectStringGeneral(dbname string) string {
+	return fmt.Sprintf(
+		"user=%s password=%s dbname=%s host=%s port=%d sslmode=disable",
+		info.user, info.password, dbname, info.host, info.port,
+	)
+}
+
+type PseDb struct {
+	db *sql.DB
+}
+
+func (pseDb PseDb) Close() {
+	pseDb.db.Close()
+}
+
+func CreateDb(info ConnectionInfo) (pseDb PseDb, err error) {
+	db, err := sql.Open("postgres", info.ConnectStringTemplateDb())
+	pseDb = PseDb{}
+
+	if err != nil {
+		return pseDb, err
+	}
+
+	_, err = db.Query(`CREATE DATABASE ` + info.dbname)
+
+	if err != nil {
+		db.Close()
+		return pseDb, err
+	}
+	db.Close()
+	return pseDb, nil
+}
+
+// func ImportCSVHistorical(file) {
+// }
--- pse_data_pg_test.go
+++ pse_data_pg_test.go
@@ -1,26 +1,88 @@
 package pse_data_pg
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/asartalo/pq"
+	"github.com/stretchr/testify/suite"
 	"testing"
-	"time"
 )
 
-func TestSettingAndGetting(t *testing.T) {
-	date, err := time.Parse("2006-01-02", "2013-05-17")
-	if err != nil {
-		t.Fatal(err)
-	}
-	row := DataRow{
-		symbol: "AUB",
-		date:   date,
-		open:   101.50000,
-		high:   105.10000,
-		low:    101.50000,
-		close:  104.00000,
-		vol:    19989300,
-	}
+var connInfo = ConnectionInfo{
+	dbname:   "pse_data_pg_test",
+	user:     "pse_test",
+	password: "pse_test",
+	host:     "localhost",
+	port:     5432,
+}
 
-	if row.date != date {
-		t.Fatalf("Date was not set for row.")
+func dropTestDb(t *testing.T) {
+	db, err := sql.Open("postgres", "user=pse_test password=pse_test dbname=postgres host=localhost port=5432 sslmode=disable")
+	if err != nil {
+		t.Fatalf(err.Error())
 	}
+	_, err = db.Query("DROP DATABASE IF EXISTS " + connInfo.dbname)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	db.Close()
+}
+
+func haltIfError(t *testing.T, msg string, err error) {
+	if err != nil {
+		t.Fatalf(fmt.Sprintf(msg+" %s", err.Error()))
+	}
+}
+
+type ConnectionInfoTestSuite struct {
+	suite.Suite
+}
+
+func (suite *ConnectionInfoTestSuite) TestConnectStringTemplateDb() {
+	expected := "user=pse_test password=pse_test dbname=template1 host=localhost port=5432 sslmode=disable"
+	suite.Equal(expected, connInfo.ConnectStringTemplateDb())
+}
+
+func (suite *ConnectionInfoTestSuite) TestConnectString() {
+	expected := "user=pse_test password=pse_test dbname=pse_data_pg_test host=localhost port=5432 sslmode=disable"
+	suite.Equal(expected, connInfo.ConnectString())
+}
+
+func TestConnectionInfoTestSuite(t *testing.T) {
+	suite.Run(t, new(ConnectionInfoTestSuite))
+}
+
+type DbTestSuite struct {
+	suite.Suite
+}
+
+func (suite *DbTestSuite) SetupTest() {
+	dropTestDb(suite.T())
+}
+
+func (suite *DbTestSuite) TearDownSuite() {
+	dropTestDb(suite.T())
+}
+
+func (suite *DbTestSuite) TestCreateNewDb() {
+	_, err := CreateDb(connInfo)
+
+	haltIfError(suite.T(), "It error'd!", err)
+
+	// Test creation
+	db, err := sql.Open("postgres", connInfo.ConnectStringTemplateDb())
+	haltIfError(suite.T(), "There's an error connecting to the test db: %s", err)
+
+	rows, err := db.Query(`SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('pse_data_pg_test')`)
+	haltIfError(suite.T(), "Error checking db: %s", err)
+
+	firstRow := rows.Next()
+	suite.NotNil(firstRow, "Database was not created")
+
+	rows.Close()
+	db.Close()
+}
+
+func TestDbTestSuite(t *testing.T) {
+	suite.Run(t, new(DbTestSuite))
 }
